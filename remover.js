d495de917270dd71ce37db7b1efe5850ec8885c4
Adrian Calthorne <39865912+AnAngryRaven@users.noreply.github.com>
2024-06-24 10:08:06 -0400

    initial code commit

---
+++ remover.js
@@ -0,0 +1,344 @@
+/**
+
+	Some notes:
+	At best, I'm an amateur coder. Combined with the fact that I don't have the most experience in JavaScript,
+	it likely means that this code isn't optimized as well or perhaps even done the "proper" way. By all means,
+	if you see a way to optimize it / just generally make it better, feel free to contribute to the GitHub repo!
+	
+	Additionally, if there are any unintended consequences to the simple approach I've taken here (I'm basically
+	just blanket deleting objects of a certain class after all!) open an issue on GitHub, and I'll take a look
+	at it.
+	
+	(I'm sure I should take a much more.. refined approach, but truth be told I'm just quickly throwing this
+	together right now so :P )
+**/
+/**
+	Copyright 2024 AnAngryRaven
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+	documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+	to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+	the Software.
+
+	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+	THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+**/
+//Viewer consts:
+const VIEWER_COUNT_SIDEBAR = `Layout-sc-1xcs6mc-0 jOVwMQ`; //View counts on the sidebar when watching a stream
+const CHANNEL_VIEWER_COUNT = `CoreText-sc-1txzju1-0 fiDbWi`; //Viewer count when watching a stream
+const BROWSE_VIEW_COUNT = `ScCoreLink-sc-16kq0mq-0 dFpxxo tw-link`; //View count when browsing categories
+const BROWSE_LIVE_VIEW_COUNT = `ScPositionCorner-sc-1shjvnv-1 kdSYzp`; //VOD view count, and view count for browsing live channels
+const BROWSE_FEATURED_LIVE_VIEW_COUNT = `CoreText-sc-1txzju1-0 ZSApR`; //View count for when previewing a stream in a category
+const BROWSE_LIST_VIEW_COUNT = `Layout-sc-1xcs6mc-0 duHgVC`; //View count when viewing live channels in a category
+const VOD_LIVEWITH_COUNT = `Layout-sc-1xcs6mc-0 fVBKtH`; //View count that appears when viewing the VOD of a currently live channel
+const CHANNEL_PAGE_VIEW_COUNT = `ScCoreLink-sc-16kq0mq-0 eFqEFL tw-link`; //"Watch now with [X] viewers" thing when looking at the about of a live channel
+const VOD_VIEW_COUNT = `Layout-sc-1xcs6mc-0 fpvLcA`;
+const MAIN_PAGE_FEATURED_STREAM = `Layout-sc-1xcs6mc-0 iBXVMz`;
+//const VOD_VIEWCOUNT = `CoreText-sc-1txzju1-0 kCftaN`; //Unused; causes other elements to disappear
+
+//Channel Leaderboard consts
+const MARQUEE_LEADERBOARD = `Layout-sc-1xcs6mc-0 gyHpt marquee-animation`; //Sliding element showing multiple people who have cheered / gifted
+const CHANNEL_LEADERBOARD = `Layout-sc-1xcs6mc-0 hsXgFK`; //General channel leaderboard
+
+//Follower / goal consts
+const FOLLOWER_COUNT = `CoreText-sc-1txzju1-0 cwNkcn`; //Follower count
+const VOD_FOLLOWER_COUNT = `Layout-sc-1xcs6mc-0 hfyuZP`; //VOD follower count
+const CHANNEL_GOAL = `Layout-sc-1xcs6mc-0 fbcEIS`; //Channel goals
+
+//Browser keys array
+const KEYS_ARR = ["viewersAll", "viewerCountSidebar", "channelViewerCount", "browseViewcount", "browseLiveViewCount", "browseFeaturedLiveViewCount", "browseListViewCount", "vodLivewithCount", "channelPageViewCount", "followerCount", "vodFollowerCount", "marqueeLeaderboard", "channelLeaderboard", "channelGoal", "vodViewCount", "mainPageFeaturedStream" ]
+
+//Preferences array; essentially caches our options so we don't have to be fetching them every time the DOM updates
+var PREF_ARR = new Array(14);
+
+const ROOT = `.tw-root--hover`;
+
+//TODO: Remove all of the "document.getElementsByClassName(`${element_remove}`)" and replace them with a function that does this and returns the value; It's unnecessarily repeating code
+
+/**Used for many things, including but not limited to:
+	- Sidebar view counts
+	- Marquee leaderboard
+	- Channel leaderboard proper
+**/
+
+async function removeElement(element_remove) {
+		
+	//Get all elements of a class
+	const viewcount_sidebar = document.getElementsByClassName(`${element_remove}`);
+	
+	//Make sure that the const actually has anything in it before we modify it!
+	if(viewcount_sidebar !== null && viewcount_sidebar !== undefined){
+		
+		//Iterate over everything in the viewcount_sidebar const
+		for(var i = 0; i < viewcount_sidebar.length; i++){				
+			//Quickly check to make sure we're not null before we delete
+			if(viewcount_sidebar[i] !== null)
+				viewcount_sidebar[i].remove();
+		}
+	}
+}
+
+/** Used mainly for the channel view count
+	This has to be used as the class for the parent of this element is used elsewhere, so there's.. unintended
+	consequences lol
+	
+	Works basically the same as the removeElement() function, just grabs the parentElement instead.
+**/
+async function removeElementParent() {
+	
+	//Get all elements of class
+	const viewcount_channel = document.getElementsByClassName(`${CHANNEL_VIEWER_COUNT}`);
+	
+	//Make sure the const has anything in before we modify it!
+	if(viewcount_channel !== null && viewcount_channel !== undefined){
+		
+		//Iterate over everything
+		for(var i = 0; i < viewcount_channel.length; i++){
+			
+			//Check to make sure we're not trying to delete a null item, then delete!
+			if(viewcount_channel[i].parentElement !== null)
+				viewcount_channel[i].parentElement.remove();
+			
+		}
+	}
+}
+
+async function removeElementWithoutAttribute(element_remove, attribute_type) {
+	
+	//Get all elements of class
+	const viewcount_channel = document.getElementsByClassName(`${element_remove}`);
+	
+	//Make sure the const has anything in before we modify it!
+	if(viewcount_channel !== null && viewcount_channel !== undefined){
+		
+		//Iterate over everything
+		for(var i = 0; i < viewcount_channel.length; i++){
+			
+			//Check to make sure we're not trying to delete a null item, then delete!
+			if(viewcount_channel[i] !== null && viewcount_channel[i].getAttribute(attribute_type) === null)
+				viewcount_channel[i].remove();
+			
+		}
+	}
+}
+
+async function removeElementWithAttribute(element_remove, attribute_type) {
+	
+	//Get all elements of class
+	const viewcount_channel = document.getElementsByClassName(`${element_remove}`);
+	
+	//Make sure the const has anything in before we modify it!
+	if(viewcount_channel !== null && viewcount_channel !== undefined){
+		
+		//Iterate over everything
+		for(var i = 0; i < viewcount_channel.length; i++){
+			
+			//Check to make sure we're not trying to delete a null item, then delete!
+			if(viewcount_channel[i] !== null && viewcount_channel[i].getAttribute(attribute_type))
+				viewcount_channel[i].remove();
+			
+		}
+	}
+}
+
+async function removeElementChild(element_remove) {
+		
+	//Get all elements of a class
+	const viewcount_sidebar = document.getElementsByClassName(`${element_remove}`);
+	
+	//Make sure that the const actually has anything in it before we modify it!
+	if(viewcount_sidebar !== null && viewcount_sidebar !== undefined){
+		
+		//Iterate over everything in the viewcount_sidebar const
+		for(var i = 0; i < viewcount_sidebar.length; i++){				
+			//Quickly check to make sure we're not null before we delete
+			if(viewcount_sidebar[i] !== null)
+				viewcount_sidebar[i].remove();
+		}
+	}
+}
+
+async function removeChildElement(element_remove, child_node_index) {
+		
+	//Get all elements of a class
+	const viewcount_sidebar = document.getElementsByClassName(`${element_remove}`);
+	
+	//Make sure that the const actually has anything in it before we modify it!
+	if(viewcount_sidebar !== null && viewcount_sidebar !== undefined){
+		
+		//Iterate over everything in the viewcount_sidebar const
+		for(var i = 0; i < viewcount_sidebar.length; i++){				
+			//Quickly check to make sure we're not null before we delete
+			if(viewcount_sidebar[i].childNodes[child_node_index] !== null && viewcount_sidebar[i].childNodes[child_node_index] !== undefined)
+				viewcount_sidebar[i].childNodes[child_node_index].remove();
+		}
+	}
+}
+
+async function setText(element_remove, inner_text) {
+	//Get all elements of a class
+	const viewcount_sidebar = document.getElementsByClassName(`${element_remove}`);
+	
+	//Make sure that the const actually has anything in it before we modify it!
+	if(viewcount_sidebar !== null && viewcount_sidebar !== undefined){
+		
+		//Iterate over everything in the viewcount_sidebar const
+		for(var i = 0; i < viewcount_sidebar.length; i++){				
+			//Quickly check to make sure we're the both the correct element and not null before we modify.
+			if(viewcount_sidebar[i] !== null && viewcount_sidebar[i].childNodes.length === 2) //Needs to be generalised, but I'm not doing that rn as it doesn't seem necessary
+				viewcount_sidebar[i].textContent = inner_text;
+		}
+	}
+}
+
+async function setTextWithAttribute(element_remove, inner_text, attribute_type, attribute_name) {
+	
+	//Get all elements of a class
+	const viewcount_sidebar = document.getElementsByClassName(`${element_remove}`);
+	
+	//Make sure that the const actually has anything in it before we modify it!
+	if(viewcount_sidebar !== null && viewcount_sidebar !== undefined){
+		//Iterate over everything in the viewcount_sidebar const
+		for(var i = 0; i < viewcount_sidebar.length; i++){				
+			//Quickly check to make sure we're not null before we delete
+			if(viewcount_sidebar[i].getAttribute(attribute_type) === attribute_name){
+				viewcount_sidebar[i].textContent = inner_text;
+				viewcount_sidebar[i].setAttribute(attribute_type, "");
+			}
+				
+		}
+	}
+}
+
+function onGot(item){
+	return item.viewersAll;
+}
+
+function onError(err) {
+	console.log("Error: ${err}");
+}
+
+async function getBrowserStorage(name){
+	var val = await chrome.storage.sync.get(name).then(result => {
+		return result[name];
+	});
+	return val;
+}
+
+//TODO: Create an array of all the storage keys, and create a for loop for this. 
+const mutationCallback = async(mutations) => {
+    mutations.forEach((mutation) => {
+		if(PREF_ARR[0]){
+			 setText(VIEWER_COUNT_SIDEBAR, "Live");
+			 removeElement(BROWSE_LIVE_VIEW_COUNT);
+			 removeElement(BROWSE_LIST_VIEW_COUNT);
+			 removeChildElement(VOD_LIVEWITH_COUNT, 1);
+			 setTextWithAttribute(CHANNEL_PAGE_VIEW_COUNT, "Watch now!", "data-a-target", "home-live-overlay-button");
+			 removeElementWithoutAttribute(BROWSE_FEATURED_LIVE_VIEW_COUNT, "style");
+			 removeElementWithAttribute(BROWSE_VIEW_COUNT, "data-a-target");
+			 removeElementParent();
+			 removeChildElement(VOD_VIEW_COUNT, 2);
+			 removeChildElement(MAIN_PAGE_FEATURED_STREAM, 2);
+			 
+			 //I suspect there's a slightly better way of doing this if I refactored a bit, but not really worried about that rn :p
+			 //I didn't go with a for loop here as there's no reason to; I also suspect it's less performant if I did do so.
+			if(PREF_ARR[9]) {
+				removeElement(FOLLOWER_COUNT);
+			}
+			if(PREF_ARR[10]){
+				removeChildElement(VOD_FOLLOWER_COUNT, 1);
+			}
+			if(PREF_ARR[11]){
+				removeElement(MARQUEE_LEADERBOARD);
+			}
+			if(PREF_ARR[12]){
+				removeElement(CHANNEL_LEADERBOARD);
+			}
+			if(PREF_ARR[13]){
+				removeElement(CHANNEL_GOAL);
+			}
+			if(PREF_ARR[14]){
+				removeChildElement(VOD_VIEW_COUNT, 2);
+			}
+			if(PREF_ARR[15]){
+				removeChildElement(MAIN_PAGE_FEATURED_STREAM, 2);
+			}
+		}else{
+			//This feels really bad, but I genuinely can't think of a better way to do this rn :<
+			//So if someone else can come up with something better.. please do >-<;
+			if(PREF_ARR[1]){
+				setText(VIEWER_COUNT_SIDEBAR, "Live");
+			}
+			if(PREF_ARR[2]){
+				
+				removeElementParent();
+			}
+			if(PREF_ARR[3]){
+				removeElementWithAttribute(BROWSE_VIEW_COUNT, "data-a-target");
+			}
+			if(PREF_ARR[4]){
+				removeElement(BROWSE_LIVE_VIEW_COUNT);
+			}
+			if(PREF_ARR[5]){
+				removeElementWithoutAttribute(BROWSE_FEATURED_LIVE_VIEW_COUNT, "style");
+			}
+			if(PREF_ARR[6]){
+				removeElement(BROWSE_LIST_VIEW_COUNT);
+			}
+			if(PREF_ARR[7]){
+				removeChildElement(VOD_LIVEWITH_COUNT, 1);
+			}
+			if(PREF_ARR[8]){
+				setTextWithAttribute(CHANNEL_PAGE_VIEW_COUNT, "Watch now!", "data-a-target", "home-live-overlay-button");
+			}
+			if(PREF_ARR[9]){
+				removeElement(FOLLOWER_COUNT);
+			}
+			if(PREF_ARR[10]){
+				removeChildElement(VOD_FOLLOWER_COUNT, 1);
+			}
+			if(PREF_ARR[11]){
+				removeElement(MARQUEE_LEADERBOARD);
+			}
+			if(PREF_ARR[12]){
+				removeElement(CHANNEL_LEADERBOARD);
+			}
+			if(PREF_ARR[13]){
+				removeElement(CHANNEL_GOAL);
+			}
+			if(PREF_ARR[14]){
+				removeChildElement(VOD_VIEW_COUNT, 2);
+			}
+			if(PREF_ARR[15]){
+				removeChildElement(MAIN_PAGE_FEATURED_STREAM, 2);
+			}
+		}
+    });
+	
+};
+async function init() {
+	const elm = document.querySelector(ROOT);
+	if (elm === null) {
+        setTimeout(init, 1000);
+        return;
+    }
+	
+	//Fetch our browser preferences on init, so that way when we're not wasting time async-ing everytime the DOM updates
+	for(var i = 0; i < PREF_ARR.length; i++){
+		PREF_ARR[i] = await getBrowserStorage(KEYS_ARR[i]);
+		//console.log(KEYS_ARR[i]);
+	}
+	
+	
+	const observer = new MutationObserver(mutationCallback);
+    const config = { childList: true, subtree: true };
+    observer.observe(elm, config);
+}
+init();
---
+++ settings.js
@@ -0,0 +1,96 @@
+/**
+	Copyright 2024 AnAngryRaven
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+	documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+	to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+	the Software.
+
+	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+	THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+**/
+const VIEWERS_ENABLE_ALL = document.getElementById("viewersAll");
+const VIEWERS_CHECKBOXES = document.getElementsByClassName("viewers-options");
+const VIEWERS_SUMMARY = document.getElementById("viewers-summary");
+const REPORT_ISSUE = document.getElementById("report-issue");
+const MIT_LICENCE = document.getElementById("mit-licence");
+
+//Browser keys array
+const KEYS_ARR = ["viewersAll", "viewerCountSidebar", "channelViewerCount", "browseViewcount", "browseLiveViewCount", "browseFeaturedLiveViewCount", "browseListViewCount", "vodLivewithCount", "channelPageViewCount", "followerCount", "vodFollowerCount", "marqueeLeaderboard", "channelLeaderboard", "channelGoal", "vodViewCount", "mainPageFeaturedStream" ]
+
+document.addEventListener("DOMContentLoaded", async (event) => {
+	
+	for(var i = 0; i < KEYS_ARR.length; i++){
+		var currentVal = await get(KEYS_ARR[i]);
+		if(currentVal === "undefined" || currentVal === undefined){
+			await chrome.storage.sync.set({[KEYS_ARR[i]]: true});
+			currentVal = await get(KEYS_ARR[i]);
+		}
+		document.getElementById(KEYS_ARR[i]).checked = currentVal;
+		
+		
+	}
+});
+
+async function get(name){
+	return await chrome.storage.sync.get(name).then(result => {
+		return result[name];
+	});
+}
+
+function onGot(item){
+	return item.viewersAll;
+}
+
+function onError(err) {
+	console.log("Error: ${err}");
+}
+
+VIEWERS_ENABLE_ALL.addEventListener("change", function() {
+	let currentState = this.checked;
+	
+	if(currentState){
+		for(var i = 0; i < VIEWERS_CHECKBOXES.length; i++){
+			VIEWERS_CHECKBOXES[i].disabled = true;
+			VIEWERS_SUMMARY.innerText = "All settings enabled!"
+			VIEWERS_SUMMARY.parentElement.open = false;
+		}
+	}else{
+		for(var i = 0; i < VIEWERS_CHECKBOXES.length; i++){
+			VIEWERS_CHECKBOXES[i].disabled = false;
+			VIEWERS_SUMMARY.innerText = "Individual settings used!"
+			VIEWERS_SUMMARY.parentElement.open = true;
+		}
+	}
+});
+
+//Only exists for browser compatibility -- for some reason, Chrome doesn't open links clicked in an extension's popup in a new tab :p
+try{
+	browser.storage.sync.get();
+}catch {
+	REPORT_ISSUE.addEventListener("click", function() {
+		window.open("https://github.com/AnAngryraven/twitch-stats-remover-extension");
+	});
+
+	MIT_LICENCE.addEventListener("click", function() {
+		window.open("https://opensource.org/license/mit");
+	});
+}
+
+
+document.addEventListener("change", async function(event) {
+	elm = event.target;
+	
+	for(var i = 0; i < KEYS_ARR.length; i++){
+		if(elm.id === KEYS_ARR[i]){
+			await chrome.storage.sync.set({[KEYS_ARR[i]]: elm.checked});
+			break;
+		}
+	}
+});
